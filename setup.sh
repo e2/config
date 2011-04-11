14b799130284d6e78388bb0877339add093e722e
Cezary Baginski <cezary.baginski@gmail.com>
2011-04-12 01:35:26 +0200

    refactor

--- setup.sh
+++ setup.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
+# URL to your dotfiles repo
+DOTFILES_URL=https://github.com/e2/my_dotfiles.git
+
+# Name of directory with dotfiles
+DOTFILES_DIR=my_dotfiles
+
+# Prefix for dotfiles, e.g "dot_" means
+# there is a file "dot_.vimrc"
+ROUTE_PREFIX=route_
+
 set -eu
 
 # make sure we are at home
 
 . ./dot_route
 
-workspace="${HOME}/workspace"
-mkdir -p "${workspace}"
-
-dotfiles="${workspace}/my_dotfiles"
+workspace="$(dirname $0)/.."
+dotfiles="${workspace}/${DOTFILES_DIR}"
 if [ ! -d "${dotfiles}" ]; then
   cd "${workspace}"
-  git clone https://github.com/e2/my_dotfiles.git
+  git clone ${DOTFILES_URL} "${DOTFILES_DIR}"
 else
   cd "${dotfiles}"
   git pull
 fi
 cd -
 
-DST_PREFIX="${dotfiles}/route_"
+DST_PREFIX="${dotfiles}/${ROUTE_PREFIX}"
 check "${DST_PREFIX}" "${HOME}/.zshenv"
 check "${DST_PREFIX}" "${HOME}/.zshrc"
 check "${DST_PREFIX}" "${HOME}/.zsh"
