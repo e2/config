689a6bc9e32b43dc35c1c543bce96c3a9907c1db
Cezary Baginski <cezary.baginski@gmail.com>
2011-04-12 18:45:58 +0200

    cleanup + add rvm install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,47 @@
 #!/bin/sh
+# {{{ Functions
+get_ruby()
+{
+  if [ ! -d "~/.rvm" ]; then
+    bash -c '"bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)"'
+    rvm install 1.9.2-head
+    rvm use --default 1.9.2-head
+    rvm use 1.9.2-head
+  fi
+}
+
+get_rake()
+{
+  get_ruby()
+}
+
+setup_latest_dotfiles()
+{
+  if [ ! -d "${dotfiles}" ]; then
+    cd "${workspace}"
+    git clone ${DOTFILES_URL} "${DOTFILES_DIR}"
+  else
+    cd "${dotfiles}"
+    git pull
+  fi
+  # generate files
+  get_rake
+  rake
+  cd -
+}
+
+restore_dotfiles()
+{
+  # Restore config files
+  DST_PREFIX="${dotfiles}/${ROUTE_PREFIX}"
+  check "${DST_PREFIX}" "${HOME}/.zshenv"
+  check "${DST_PREFIX}" "${HOME}/.zshrc"
+  check "${DST_PREFIX}" "${HOME}/.zsh"
+  check "${DST_PREFIX}" "${HOME}/.vimrc"
+  check "${DST_PREFIX}" "${HOME}/.gitconfig"
+}
+
+#}}}
 
 # URL to your dotfiles repo
 DOTFILES_URL=https://github.com/e2/my_dotfiles.git
@@ -12,31 +55,12 @@ ROUTE_PREFIX=route_
 
 set -eu
 
-# make sure we are at home
-
 . ./dot_route
 
-# Download/update repo
 workspace="$(dirname $0)/.."
 dotfiles="${workspace}/${DOTFILES_DIR}"
 
-if [ ! -d "${dotfiles}" ]; then
-  cd "${workspace}"
-  git clone ${DOTFILES_URL} "${DOTFILES_DIR}"
-else
-  cd "${dotfiles}"
-  git pull
-fi
-# generate files
-rake
-cd -
-
-# Restore config files
-DST_PREFIX="${dotfiles}/${ROUTE_PREFIX}"
-check "${DST_PREFIX}" "${HOME}/.zshenv"
-check "${DST_PREFIX}" "${HOME}/.zshrc"
-check "${DST_PREFIX}" "${HOME}/.zsh"
-check "${DST_PREFIX}" "${HOME}/.vimrc"
-check "${DST_PREFIX}" "${HOME}/.gitconfig"
+setup_latest_dotfiles
+restore_dotfiles
 
 # vi:fdm=marker:
